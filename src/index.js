9a166aa05e388202700db741b3b7459f741e713f
Alejandro Palmar <thepalmars1one@gmail.com>
2023-02-22 16:41:28 -0400

    movie request and genres schema created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const moviesRoutes = require("./routes/movies")
+const genresRoutes = require("./routes/genres")
 
 const app = express();
 const port = process.env.PORT || 9000;
@@ -9,6 +10,7 @@ const port = process.env.PORT || 9000;
 // middleware
 app.use(express.json());
 app.use("/api", moviesRoutes);
+app.use("/api", genresRoutes);
 
 // routes
 app.listen(port, () => console.log("Server listening on port", port));
